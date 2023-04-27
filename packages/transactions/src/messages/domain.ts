260c58f256ec83d5f14e2a605c020aea3e18334d
trevormil <trevormil@comcast.net>
2023-04-27 08:57:11 -0400

    fix salt length

--- packages/transactions/src/messages/domain.ts
+++ packages/transactions/src/messages/domain.ts
@@ -4,6 +4,6 @@ export function getDefaultDomainWithChainId(chainId: number) {
         version: '1.0.0',
         chainId,
         verifyingContract: '0x1a16c87927570239fecd343ad2654fd81682725e',
-        salt: '0x5d1e2c0e9b8a5c395979525d5f6d5f0c595d5a5c5e5e5b5d5ecd5a5e5d2e54',
+        salt: '0x5d1e2c0e9b8a5c395979525d5f6d5f0c595d5a5c5e5e5b5d5ecd5a5e5d2e5412',
     }
 }
