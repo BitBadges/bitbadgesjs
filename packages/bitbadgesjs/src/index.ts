693e4ffe373dbd2e9d3e0872b1e945b972023fb8
Trevor Miller <trevormil@comcast.net>
2023-11-27 12:00:42 -0500

    gendocs

--- packages/bitbadgesjs/src/index.ts
+++ packages/bitbadgesjs/src/index.ts
@@ -1,6 +1,4 @@
-import * as addressConverter from 'bitbadgesjs-address-converter'
 import * as proto from 'bitbadgesjs-proto'
-import * as provider from 'bitbadgesjs-provider'
 import * as utils from 'bitbadgesjs-utils'
 
-export { addressConverter, proto, provider, utils }
+export {  proto, utils }
