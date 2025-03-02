e596e9200eb54587ca253107fc685facdc601ab7
Trevor Miller <trevormil@comcast.net>
2025-03-01 19:56:44 -0500

    /

--- packages/bitbadgesjs-sdk/scripts/gen_api_docs_ci.sh
+++ packages/bitbadgesjs-sdk/scripts/gen_api_docs_ci.sh
@@ -7,8 +7,8 @@ sed -i 's/"sideEffects": false,/"sideEffects": false,\n  "type": "module",/' pac
 
 source ./scripts/combine_ts_files.sh
 tsx ./scripts/normalize_combined.ts ./src/combined.ts
-npm run format
+npm run format || echo "Format failed, continuing anyway..."
 source ./scripts/create_yml_schemas.sh
-npm run format
+npm run format || echo "Format failed, continuing anyway..."
 tsx ./scripts/normalize_yml.ts ./openapitypes/combined.yaml
 rm ./src/combined.ts
