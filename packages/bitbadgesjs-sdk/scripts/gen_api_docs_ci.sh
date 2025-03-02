a16e41c87431e94d2de02baf0e4dad2338954237
Trevor Miller <trevormil@comcast.net>
2025-03-01 19:51:09 -0500

    .

--- packages/bitbadgesjs-sdk/scripts/gen_api_docs_ci.sh
+++ packages/bitbadgesjs-sdk/scripts/gen_api_docs_ci.sh
@@ -5,16 +5,17 @@ echo "No changes found. Proceeding..."
 # set package.json type to module
 sed -i 's/"sideEffects": false,/"sideEffects": false,\n  "type": "module",/' package.json
 
-ts-node ./scripts/check_routes_consistency.ts ../../../bitbadges-indexer/src/indexer.ts  ./openapitypes-helpers/routes.yaml
+# Use tsx instead of ts-node for better ESM support
+tsx ./scripts/check_routes_consistency.ts ../../../bitbadges-indexer/src/indexer.ts  ./openapitypes-helpers/routes.yaml
 if [ $? -ne 0 ]; then
     echo "Route consistency check failed!"
     # exit 0
 fi
 
 source ./scripts/combine_ts_files.sh
-ts-node ./scripts/normalize_combined.ts ./src/combined.ts
+tsx ./scripts/normalize_combined.ts ./src/combined.ts
 npm run format
 source ./scripts/create_yml_schemas.sh
 npm run format
-ts-node ./scripts/normalize_yml.ts ./openapitypes/combined.yaml
+tsx ./scripts/normalize_yml.ts ./openapitypes/combined.yaml
 rm ./src/combined.ts
