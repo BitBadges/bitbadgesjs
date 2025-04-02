51a8d3dff91ef78c5d53538503188d1ae75b1292
Trevor Miller <trevormil@comcast.net>
2025-04-02 14:21:26 -0400

    fix auth?

--- packages/bitbadgesjs-sdk/scripts/bump_python_sdk.sh
+++ packages/bitbadgesjs-sdk/scripts/bump_python_sdk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Clean up old build artifacts with proper permissions
 sudo rm -rf ./sdk-python dist/ build/ *.egg-info/
 
@@ -33,10 +34,20 @@ python -m build
 
 pytest
 
-# Upload to PyPI using API token authentication
-python -m twine upload dist/* \
-    --username "__token__" \
-    --password "${PYPI_TOKEN}"
+
+
+# Create .pypirc file with token authentication
+cat > "$HOME/.pypirc" << EOF
+[pypi]
+username = __token__
+password = ${PYPI_TOKEN}
+EOF
+
+# Upload to PyPI (now using .pypirc config)
+python -m twine upload dist/*
+
+# Clean up the credentials file
+rm "$HOME/.pypirc"
 
 
 cd ../
